f7ffb0f5e6baf2eb2dda301253ee1740c2755b40
Chris Cedeno <chrisrey95@gmail.com>
2019-02-23 20:24:46 -0500

    added notes helper functions for db

---
+++ server/database/helpers/notes.js
@@ -0,0 +1,42 @@
+const db = require('../dbConfig');
+
+const getNotes = userId => {
+    return db('notes').where('user_id', '=', userId)
+}
+
+const getTags = noteId => {
+    return db('tags as t')
+        .join('note_tags as n', 'n.note_id', 't.id')
+        .where({'n.note_id': noteId})
+}
+
+const getNoteById = (noteId, userId) => {
+    return db('notes')
+        .whereIn(['id', 'user_id'], [[noteId, userId]])
+        .first();
+}
+
+const createNote = note => {
+    return db('notes').returning('id').insert(note)
+}
+
+const editNote = (noteId, userId, data) => {
+    return await db('notes')
+        .whereIn(['id', 'user_id'], [[noteId, userId]])
+        .update(data);
+}
+
+const deleteNote = (noteId, userId) => {
+    return b('notes')
+        .whereIn(['id', 'user_id'], [[noteId, userId]])
+        .del();
+}
+
+module.exports = {
+    getNotes,
+    getTags,
+    getNoteById,
+    createNote,
+    editNote,
+    deleteNote
+}
